84dc97939bb6ab3755ad72746fd51304829bf2f8
Adam Norris <acnorrisuk@gmail.com>
2017-06-11 20:20:35 +0100

    Add caesers-cipher.js

--- Basic Algorithm Scripting/caesers-cipher.js
+++ Basic Algorithm Scripting/caesers-cipher.js
@@ -2,8 +2,24 @@
 
 function rot13(str) { // LBH QVQ VG!
   
-  return str;
+  const rot = 13;
+
+    const newStrArr = [];
+
+    for( let i = 0; i < str.length; i++ ) {
+      if(str.charCodeAt(i) >= (65 + rot) && str.charCodeAt(i) <=90 ) {
+        newStrArr.push(str.charCodeAt(i) - rot);
+      } else if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <=(65 + rot) ) {
+        newStrArr.push(str.charCodeAt(i) + rot);
+      } else {
+          newStrArr.push(str.charCodeAt(i));
+      }
+  }
+
+  // return new letters
+  return String.fromCharCode(...newStrArr);
+
 }
 
 // Change the inputs below to test
-rot13("SERR PBQR PNZC");
+console.log(rot13("SERR PBQR PNZC"));
