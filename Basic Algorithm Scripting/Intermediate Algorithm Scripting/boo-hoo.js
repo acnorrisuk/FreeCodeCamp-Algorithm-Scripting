261e8ca9f0b1b76c2b3f194ef07ea2d7613fddd8
Adam Norris <acnorrisuk@gmail.com>
2017-06-18 15:52:53 +0100

    Add boo-hoo.js

---
+++ Basic Algorithm Scripting/Intermediate Algorithm Scripting/boo-hoo.js
@@ -0,0 +1,8 @@
+// https://www.freecodecamp.com/challenges/boo-who
+
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  return typeof(bool) === 'boolean';
+}
+
+console.log(booWho(false));
