3f4e23fa0e39bc1bc3b9eb7a20eb60e35bd93809
Adam Norris <acnorrisuk@gmail.com>
2017-06-18 16:17:04 +0100

    Add everything-be-true.js

---
+++ Basic Algorithm Scripting/Intermediate Algorithm Scripting/everything-be-true.js
@@ -0,0 +1,10 @@
+// https://www.freecodecamp.com/challenges/everything-be-true
+
+function truthCheck(collection, pre) {
+  // Is everyone being true?
+  return collection.every(function(obj){
+    return obj[pre];
+  });
+}
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
